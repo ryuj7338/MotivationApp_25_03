a5e0d062fa00b127e58a43eb5f3d27d5d31f7257
ryuj7338 <ryuj7338@gmail.com>
2025-03-21 13:46:56 +0900

    motivation 리스트 나오게하기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,8 @@ package org.example;
 
 
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -16,7 +18,10 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
 
-        int id = 1;
+        int Id = 0;
+
+
+        List<Motivation> motivations = new ArrayList<Motivation>();
 
         String cmd;
         while (true) {
@@ -31,22 +36,72 @@ public class Main {
             }
 
             if (cmd.equals("add")) {
-                System.out.print("motivation : ");
-                String motivation = sc.nextLine().trim();
+                int id = Id + 1;
+                System.out.print("body : ");
+                String body = sc.nextLine();
                 System.out.print("source : ");
-                String source = sc.nextLine().trim();
-                System.out.printf("%d번 motivation이 출력되었습니다.\n", id);
-                id++;
+                String source = sc.nextLine();
+
+                Motivation motivation = new Motivation(id, body, source);
+
+                motivations.add(motivation);
+
+
+                System.out.printf("%d번 motivation이 등록되었습니다.\n", id);
+                Id++;
+
             }else if(cmd.equals("list")) {
                 System.out.println("=".repeat(40));
                 System.out.printf("   번호    /     source      /      motivation        \n");
+                for (int i = motivations.size() - 1; i >= 0; i--) {
+                    Motivation motivation = motivations.get(i);
+                    System.out.printf("   %d   /   %s   /   %s   \n", motivation.getId(), motivation.getBody(), motivation.getSource());
+
+                }
+
+                System.out.println("=".repeat(40));
             }
         }
     }
 }
-class Moti {
+class Motivation {
     int id;
-    String motivation;
+    String body;
     String source;
 
+    public Motivation(int id, String body, String source) {
+        this.id = id;
+        this.body = body;
+        this.source = source;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+    @Override
+    public String toString() {
+        id++;
+        return "     " + id + "   /  " + "     " +  body + "    /  "  +"     "+ source  ;
+    }
 }
